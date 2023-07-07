be58f902135ad6188971070c77553174db54d25e
Shadat <shadat.rahman.464@gmail.com>
2023-07-07 23:25:25 +0600

    counter is working!!

--- CounterApp/ContentView.swift
+++ CounterApp/ContentView.swift
@@ -2,18 +2,33 @@
 //  ContentView.swift
 //  CounterApp
 //
-//  Created by Al Rasin on 7/7/23.
+//  Created by Shadat Rahman on 7/7/23.
 //
 
 import SwiftUI
 
+
+
 struct ContentView: View {
+    @State private var counter = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(counter)").font(.system(size: 30))
+            HStack{
+                Button(action: {
+                    counter -= 1
+                }){
+                    Label("", systemImage: "minus").font(.title).labelStyle(.iconOnly)
+                }
+                Spacer()
+                Button(action: {
+                    counter += 1
+                }){
+                    Label("", systemImage: "plus").font(.title).labelStyle(.iconOnly)
+                }
+                
+            }.padding(.leading, 100).padding(.trailing, 100).padding(.top, 10)
+            
         }
         .padding()
     }
