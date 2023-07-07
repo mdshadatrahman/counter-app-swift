f753d321c9703a98548d570c6d5f862540c16bfc
Shadat <shadat.rahman.464@gmail.com>
2023-07-07 22:58:39 +0600

    Initial Commit

---
+++ CounterApp/CounterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterAppApp.swift
+//  CounterApp
+//
+//  Created by Al Rasin on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CounterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
